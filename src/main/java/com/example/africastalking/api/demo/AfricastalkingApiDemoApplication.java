b4144e07f17901d3ad3acc3e77bb8ea7eb50ab54
wickliff adero <aderowickliff8@gmail.com>
2025-03-19 15:11:55 +0300

    created spring boot to intaract with africastalking API

---
+++ src/main/java/com/example/africastalking/api/demo/AfricastalkingApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.africastalking.api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricastalkingApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfricastalkingApiDemoApplication.class, args);
+	}
+
+}
